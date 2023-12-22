44705abc4859ec661679a769b361c9f0ef6bf60b
chetan-kasar <116253410+chetan-kasar@users.noreply.github.com>
2023-12-22 22:58:07 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,43 +1,14 @@
 // server.js
 const express = require('express');
 const bodyParser = require('body-parser');
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://kasarschetan1122:Yhcs4f5RBWwKqmU2@cluster13.x1rw8bv.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri);
-const cors = require('cors');
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use(cors(
-    {
-        origin:["https://finalfk-frontend.vercel.app"],
-        methods:["POST","GET"],
-        credentials:true
-    }
-));
-
-app.get("/",(req,res)=>{
-    res.end("Hello");
-})
-
-app.post('/add', async (req, res) => {
-
-    const database = client.db("mydb2");
-    const mycollection = database.collection("mycollection");
-    const result = await mycollection.insertOne(req.body.userData);
-    console.log(req.body.userData);
-
-    // console.log(global.imageName);
-
-    //res.json({ message: 'Data received on the server' });
+app.use("/",(req, res)=>{
+  res.json({message:"Hello"});
 });
 
-app.post('/api/username', (req, res) => {
-  console.log('Received username:', req.body.userData.username);
-  res.json({ message: 'Username received on the server' });
-});
-
-app.listen(3001, () => {
-  console.log("Server running");
+app.listen(9000, () => {
+  console.log(`Server running`);
 });
